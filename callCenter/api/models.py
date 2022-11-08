747c9daec624f47ebcee09e511d48005c2c9344c
nadaelrifai44 <nadaelrifai44@gmail.com>
2022-11-08 10:04:31 +0200

    add register models to admin.py

--- callCenter/api/models.py
+++ callCenter/api/models.py
@@ -6,11 +6,13 @@ from django.db import models
 class Customer(models.Model):
     firstName = models.CharField(max_length=200, null=False)
     lastName = models.CharField(max_length=200, null=False)
+    address = models.ForeignKey('Address', null=True, on_delete=models.CASCADE)
+    phone = models.ForeignKey('Phone', null=True, on_delete=models.CASCADE)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.firstName, self.lastName
+        return self.firstName
 
 class Address(models.Model):
     address=models.CharField(max_length=300)
@@ -20,7 +22,12 @@ class Address(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True) 
 
-class phone(models.Model):
+    def __str__(self):
+        return self.address
+
+class Phone(models.Model):
     phone = models.CharField(max_length=200)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)      
+    def __str__(self):
+        return self.phone
